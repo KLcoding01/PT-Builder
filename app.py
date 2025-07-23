c3159a58dceee2d731da78783ba79600abd5819e
Kelvin Lam <kelvin@Kelvins-MacBook-Pro.local>
2025-07-22 23:33:26 -0700

    Change SQLite db path for Render

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from models import db, Therapist
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "dev_secret_key_change_me")
 basedir = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(basedir, 'static', 'uploads', 'db.sqlite3')
+db_path = '/tmp/db.sqlite3'
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
